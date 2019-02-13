05fc1a6b4c9ff29808d7b09aaa7babdc6d6ca8b3
Cody Spate <codyspate@gmail.com>
2019-02-13 11:49:44 -0800

    feat: babel config updates

--- src/shared/resolvers.js
+++ src/shared/resolvers.js
@@ -1,5 +1,12 @@
 export default {
   Query: {
-    hello: () => 'Hello World via GraphQL'
+    hello: (_, { name = 'World' }) => {
+      const obj = {
+        message: `Hello ${name} via GraphQL`,
+        num: 10,
+        messages: ['Hello', name, 'via', 'GraphQL']
+      };
+      return { ...obj, obj };
+    }
   }
 };
--- src/shared/type-defs.js
+++ src/shared/type-defs.js
@@ -1,7 +1,13 @@
 import { gql } from 'apollo-server-cloud-functions';
 
 export default gql`
+  type HelloObj {
+    message: String
+    obj: HelloObj
+    messages: [String]
+    num: Int
+  }
   type Query {
-    hello: String
+    hello(name: String): HelloObj
   }
 `;
